1e4ed89d3db5a8b25f176393bf2c767c0f2a991b
Anders-QC <qiujianzhong888@gmail.com>
2023-12-07 11:18:58 +0800

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -144,6 +144,8 @@ function main() {
         echoInfo "Upgrade httprunner..."
         echo "$ $HOME/.hrp/venv/bin/pip3 install --upgrade httprunner==$version --index-url https://pypi.org/simple"
         $HOME/.hrp/venv/bin/pip3 install --upgrade httprunner==$version --index-url https://pypi.org/simple
+        $HOME/.hrp/venv/bin/pip3 install requests
+        $HOME/.hrp/venv/bin/pip3 install fake-useragent
     fi
 }
 
